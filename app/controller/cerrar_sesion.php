c03278d8d6217ec73fc5842fe29eed602068d44b
jesuarman02 <jesuarman74@gmail.com>
2024-10-27 21:11:01 -0600

    Actualizacion Editar y Cerrar Sesion

--- app/controller/cerrar_sesion.php
+++ app/controller/cerrar_sesion.php
@@ -1,12 +1,8 @@
 <?php
 session_start();
-session_unset();
 session_destroy();
 
-if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) === 'xmlhttprequest') {
-    echo json_encode(['success' => true, 'message' => 'Sesión cerrada con éxito.']);
-} else {
-    header("Location: ../../login.php");
-    exit();
-}
+// Redirigir al login después de cerrar sesión
+header("Location: ../../login.php"); // Asegúrate de que la ruta sea correcta
+exit;
 ?>
--- public/js/cerrar_session.js
+++ public/js/cerrar_session.js
@@ -1,17 +1,11 @@
 const cerrar_sesion = () => {
-    fetch("app/controller/cerrar_sesion.php")
-    .then(respuesta => respuesta.json())
-    .then(async (respuesta) => {
-        if (respuesta.success) {
-            await Swal.fire({ icon: "success", title: 'Sesión Finalizada' });
-            window.location.href = "http://localhost/suits/unidad2/proyecto/login.php"; // Ruta absoluta
-        } else {
-            await Swal.fire({ icon: "error", title: 'Error al cerrar sesión' });
-        }
-    })
-    .catch((error) => {
-        console.error("Error al cerrar sesión:", error);
-        Swal.fire({ icon: "error", title: 'Error en la conexión al cerrar sesión' });
+    Swal.fire({ 
+        icon: "success", 
+        title: 'Sesión cerrada con éxito', 
+        timer: 1000, 
+        showConfirmButton: false 
+    }).then(() => {
+        window.location.href = "app/controller/cerrar_sesion.php"; 
     });
 }
 
